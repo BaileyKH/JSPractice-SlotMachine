1c40aedcbcc1647359673f9449592aaa54ad0cbf
Bailey <hen.baileyk@gmail.com>
2023-08-19 13:43:13 -0700

    Added the ability to place a bet considering the users balance and the number of lines they plan to bet on

--- slotmachine.js
+++ slotmachine.js
@@ -34,5 +34,20 @@ const  getNumberOfLines = () => {
         }
 };
 
-const depositAmount = deposit();
+const getBet = (balance, lines) => {
+    while (true) {
+        const bet = prompt('Enter your total Bet per line): ')
+            const numberBet = parseFloat(bet);
+    
+            if (isNaN(numberBet) || numberBet <= 0 || numberBet > balance / lines) {
+                console.log("Invalid bet, try again!")
+            } else {
+                return numberBet;
+            }
+        }
+}
+
+let balance = deposit();
 const numberOfLines = getNumberOfLines();
+const bet = getBet(balance, numberOfLines);
+
